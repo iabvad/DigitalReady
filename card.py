383849ce843fb36c76f6fe2e20b3a95f82c87bd0
BryceDigitalReady <147752604+BryceDigitalReady@users.noreply.github.com>
2023-11-16 16:24:05 -0500

    Update card.py

--- card.py
+++ card.py
@@ -1,21 +1,26 @@
 import sys, random
+# The main idea of the Class below allows us to form the Basics of the whole code and how we will be able 
+# to tell the difference between the values, players and face (Which will differ based on face).
 
 class Card:
-    def __init__(self, face: str, value: int, player: str):
+    def __init__(self, face: list, value: list, player: str):
         self.face = face
         self.value = value
         self.player = player
 
+    def pick_a_card(): 
+        card = random.choices(cards) 
+        rank = random.choices(ranks) 
+        return "The " + rank + " of " + card
+
+# These Objects will tell us which cards will be used in the game, based on the card and its type, we can 
+# determine the value and add it to the total count.
 cards = ["♦️", "♠️", "♥️", "♣️"] 
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 
 
 
-
-def pick_a_card(): 
-    card = random.choices(cards) 
-    rank = random.choices(ranks) 
-    return "The " + rank + " of " + card 
+# How the code will pick a randomized card to be played.
 
 def go(num_players: int):
 
